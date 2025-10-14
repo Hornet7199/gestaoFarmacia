8baa70bbcb633e9547772a22ebd97b08f5d2ef19
RODRIGO DE MELLO CAVALCANTE <rodrigo_cavalcant150@estudante.sesisenai.org.br>
2025-10-14 20:48:43 -0300

    Criado

---
+++ src/main/java/projeto/farmaciaSenai/FarmaciaSenaiApplication.java
@@ -0,0 +1,13 @@
+package projeto.farmaciaSenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaSenaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projeto/farmaciaSenai/FarmaciaSenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.farmaciaSenai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaSenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
